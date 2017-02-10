159ce6168ad950f42253da7e0cd3dc4cfde7ab7d
Scott Numamoto <scott.numamoto@gmail.com>
2017-02-10 00:22:04 -0800

    Improve camera

--- kiwi.py
+++ kiwi.py
@@ -43,56 +43,102 @@ class Robot:
             self.Wl = -speed * 3
         elif device == "right_motor":
             self.Wr = speed * 3
-        else :
+        else:
             raise KeyError("Cannot find device name: " + device)
 
-class Wheel:
-    base = list("*---*|   || x ||   |*---*")
 
-    def __init__ (self, robot):
+class Camera:
+    wheel_base = list("* - - - *|       ||   x   ||       |* - - - *")
+    width = 9
+    base = list(" " * (5 * width))
+
+    def __init__(self, robot, gamepad):
         self.robot = robot
+        self.gamepad = gamepad
 
-    def img(self):
-        """Returns a list of the component strings based on angle"""
-        theta = self.robot.ltheta
-
-        result = list(Wheel.base)
+    def direction(theta):
+        result = Camera.base.copy()
         state = round(theta / 45.0) % 8
 
-        assert state != 8
+        result[2 * Camera.width + 4] = "*"
 
         if state == 0:
-            result[1 * 5 + 2] = "|"
+            result[0 * Camera.width + 4] = "|"
+            result[1 * Camera.width + 4] = "|"
         elif state == 1:
-            result[1 * 5 + 3] = "/"
+            result[0 * Camera.width + 8] = "/"
+            result[1 * Camera.width + 6] = "/"
         elif state == 2:
-            result[2 * 5 + 3] = "-"
+            result[2 * Camera.width + 5] = "-"
+            result[2 * Camera.width + 6] = "-"
+            result[2 * Camera.width + 7] = "-"
         elif state == 3:
-            result[3 * 5 + 3] = "\\"
+            result[3 * Camera.width + 6] = "\\"
+            result[4 * Camera.width + 8] = "\\"
         elif state == 4:
-            result[3 * 5 + 2] = "|"
+            result[3 * Camera.width + 4] = "|"
+            result[4 * Camera.width + 4] = "|"
         elif state == 5:
-            result[3 * 5 + 1] = "/"
+            result[3 * Camera.width + 2] = "/"
+            result[4 * Camera.width + 0] = "/"
         elif state == 6:
-            result[2 * 5 + 1] = "-"
+            result[2 * Camera.width + 0] = "-"
+            result[2 * Camera.width + 1] = "-"
+            result[2 * Camera.width + 2] = "-"
         elif state == 7:
-            result[1 * 5 + 1] = "\\"
-        return result
-
-    def printer(self,listy):
-        for y in range(5):
-            segment = listy[y * 5:(y + 1) * 5]
-            print(' '.join(segment))
-
-class MenuBar:
-    """Contains  mostly nongraphic stats"""
-
-    def __init__(self, robot):
-        self.robot = robot
+            result[0 * Camera.width + 0] = "\\"
+            result[1 * Camera.width + 2] = "\\"
+        return Camera.str_format(result)
 
     def robot_direction(self):
-        self.robot.dir
-        return
+        return Camera.direction(self.robot.dir)
+
+    def left_joystick(self):
+        return Camera.direction(self.gamepad.ltheta())
+
+    def right_joystick(self):
+        return Camera.direction(self.gamepad.rtheta())
+
+    def wheel(theta):
+        result = Camera.wheel_base.copy()
+        state = round(theta / 45.0) % 8
+
+        if state == 0:
+            result[1 * Camera.width + 4] = "|"
+        elif state == 1:
+            result[1 * Camera.width + 7] = "/"
+        elif state == 2:
+            result[2 * Camera.width + 7] = "-"
+        elif state == 3:
+            result[3 * Camera.width + 7] = "\\"
+        elif state == 4:
+            result[3 * Camera.width + 4] = "|"
+        elif state == 5:
+            result[3 * Camera.width + 1] = "/"
+        elif state == 6:
+            result[2 * Camera.width + 1] = "-"
+        elif state == 7:
+            result[1 * Camera.width + 1] = "\\"
+
+        return Camera.str_format(result)
+
+    def right_wheel(self):
+        return Camera.wheel(self.robot.rtheta)
+
+    def left_wheel(self):
+        return Camera.wheel(self.robot.ltheta)
+
+    def str_format(list_img):
+        """Returns a list of 5 strings of length 9"""
+        result = []
+        for y in range(5):
+            segment = list_img[y * Camera.width:(y + 1) * Camera.width]
+            result.append(''.join(segment))
+        return result
+
+    def printer(formatted_list):
+        for x in formatted_list:
+            print(x)
 
 
 class Screen:
@@ -100,29 +146,49 @@ class Screen:
     SCREEN_HEIGHT = 48
     SCREEN_WIDTH = 48
 
-    def __init__(self, robot):
+    def __init__(self, robot, gamepad):
         self.robot = robot
+        self.gamepad = gamepad
+        self.camera = Camera(self.robot, self.gamepad)
+
+    def combiner(parts_list):
+        result = []
+        for y in range(5):
+            pre_segment = []
+            for x in range(len(parts_list)):
+                pre_segment.append(parts_list[x][y])
+            line_str = ''.join(pre_segment)
+            result.append(line_str)
+        return result
+
+    def menu_bar(self):
+        menu_bar_items = []
+        menu_bar_items.append(self.camera.right_wheel())
+        menu_bar_items.append(self.camera.left_wheel())
+        menu_bar_items.append(self.camera.robot_direction())
+        Camera.printer(Screen.combiner(menu_bar_items))
 
     def draw(self):
-        for y in range(Screen.SCREEN_HEIGHT):
+        # print the menu bar
+        self.menu_bar()
+
+        # print the map
+        for y in reversed(range(Screen.SCREEN_HEIGHT)):
             line = ["."] * Screen.SCREEN_WIDTH
             for x in range(Screen.SCREEN_WIDTH):
                 if (self.robot.X // 1 == x and self.robot.Y // 1 == y):
                     line[x] = "@"
             print(' '.join(line))
-        print("__"* Screen.SCREEN_WIDTH)
+        print("__" * Screen.SCREEN_WIDTH)
 
 if __name__ == "__main__":
     r = Robot()
-    w = Wheel(r)
-    s = Screen(r)
+    r.set_value("left_motor", 1)
+    r.set_value("right_motor", .9)
+    s = Screen(r, "place holder for gamepad")
+
     while True:
-        r.set_value("left_motor", 1)
-        r.set_value("right_motor", 0)
         r.update_position()
         s.draw()
-        w.printer(w.img())
-        # print "X = " + str(r.X)
-        # print "Y = " + str(r.Y)
-        # print r.ltheta
         time.sleep(r.tick_rate)
+
