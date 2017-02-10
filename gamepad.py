e588e5df78b272976caa07a8e89abbd57ef41f94
chuen0630 <WingChuenLam@pioneers.berkeley.edu>
2017-02-10 01:17:50 -0800

    added some comments

--- gamepad.py
+++ gamepad.py
@@ -7,13 +7,13 @@ class Gamepad:
         self.joystick_right_y = 0
 
     def get_value(self, device):
-        if (device = "joystick_left_x"):
+        if (device == "joystick_left_x"):
             return self.joystick_left_x
-        if (device = "joystick_left_y"):
+        if (device == "joystick_left_y"):
             return self.joystick_left_y
-        if (device = "joystick_right_x"):
+        if (device == "joystick_right_x"):
             return self.joystick_right_x
-        if (device = "joystick_right_y"):
+        if (device == "joystick_right_y"):
             return self.joystick_right_y
         else:
             raise KeyError("Cannot find input: " + deivce)
@@ -22,13 +22,13 @@ class Gamepad:
     def godmode(self, input, value):
         if value > 1.0 or value < -1.0:
             raise ValueError("Value cannot be great than 1.0 or less than -1.0.")
-        if (device = "joystick_left_x"):
+        if (device == "joystick_left_x"):
             self.joystick_left_x = value
-        elif (device = "joystick_left_y"):
+        elif (device == "joystick_left_y"):
             self.joystick_left_y = value
-        elif (device = "joystick_right_x"):
+        elif (device == "joystick_right_x"):
             self.joystick_right_x = value
-        elif (device = "joystick_right_y"):
+        elif (device == "joystick_right_y"):
             self.joystick_right_y = value
         else:
             raise KeyError("Cannot find input: " + deivce)
@@ -39,7 +39,7 @@ class Gamepad:
         if (x == 0 and y == 0):
             return "Neutral"
         theta = math.degrees(math.atan(y / x))
-        if x = 0:
+        if x == 0:
             if y > 0:
                 return 90.0
             else:
@@ -54,7 +54,7 @@ class Gamepad:
         if (x == 0 and y == 0):
             return "Neutral"
         theta = math.degrees(math.atan(y / x))
-        if x = 0:
+        if x == 0:
             if y > 0:
                 return 90.0
             else:
--- kiwi.py
+++ kiwi.py
@@ -6,11 +6,13 @@ JOYSTICK_NEUTRAL = "Neutral"
 
 
 class Robot:
-    tick_rate = 0.1            # in s
-    width = 12                   # width of robot
-    w_radius = 2               # radius of a wheel
-    MAX_X = 143
-    MAX_Y = 143
+    """The MODEL for this simulator. Stores robot data and handles position
+       calculations & Runtime API calls """
+    tick_rate = 0.1             # in s
+    width = 12                  # width of robot , inches
+    w_radius = 2                # radius of a wheel, inches
+    MAX_X = 143                 # maximum X value, inches, field is 12'x12'
+    MAX_Y = 143                 # maximum Y value, inches, field is 12'x12'
     neg = -1                    # negate left motor calculation
 
     def __init__(self):
@@ -18,14 +20,15 @@ class Robot:
         self.Y = 72.0           # Y position of the robot
         self.Wl = 0.0           # angular velocity of l wheel, degree/s
         self.Wr = 0.0           # angular velocity of r wheel, degree/s
-        self.ltheta = 0.0       # angular position of l wheel, in degree
-        self.rtheta = 0.0       # angular position of r wheel, in degree
-        self.dir = 90.0           # Direction of the robot facing,in degree
+        self.ltheta = 0.0       # angular position of l wheel, degree
+        self.rtheta = 0.0       # angular position of r wheel, degree
+        self.dir = 90.0         # Direction of the robot facing, degree
 
-    """ DifferentialDrive Calculation Credits to:
+    """ Differential Drive Calculation Reference:
     https://chess.eecs.berkeley.edu/eecs149/documentation/differentialDrive.pdf
     """
     def update_position(self):
+        """Updates position of the  Robot using differential drive equations"""
         lv = self.Wl * Robot.w_radius * Robot.neg
         rv = self.Wr * Robot.w_radius
         radian = math.radians(self.dir)
@@ -44,13 +47,14 @@ class Robot:
             dx = i * math.sin(radian) + j * math.cos(radian)
             dy = i * math.cos(radian) + j * math.sin(radian)
             self.dir = (self.dir + math.degrees(theta)) % 360
-
         self.X = max(min(self.X + dx, Robot.MAX_X), 0)
         self.Y = max(min(self.Y + dy, Robot.MAX_Y), 0)
         self.ltheta = (self.Wl * 5 + self.ltheta) % 360
         self.rtheta = (self.Wr * 5 + self.rtheta) % 360
 
     def set_value(self, device, speed):
+        """Runtime API method for updating L/R motor speed. Takes only L/R
+           Motor as device name and speed bounded by [-1,1]."""
         if speed > 1.0 or speed < -1.0:
             raise ValueError("Speed cannot be great than 1.0 or less than -1.0.")
         if (device == "left_motor"):
@@ -184,7 +188,7 @@ class Screen:
     def __init__(self, robot, gamepad):
         self.robot = robot
         self.gamepad = gamepad
-        self.camera = Camera(self.robot, self.gamepad)
+        self.camera = Camera(robot, gamepad)
 
     def combiner(parts_list):
         """Creates a list of 5 strings that make up the menu_bar"""
@@ -219,11 +223,12 @@ class Screen:
 
 if __name__ == "__main__":
     r = Robot()
-    r.set_value("left_motor", 1)
-    r.set_value("right_motor", .9)
-    s = Screen(r, "place holder for gamepad")
+    g = Gamepad()
+    s = Screen(r, g)
 
     while True:
+        r.set_value("left_motor", 1)
+        r.set_value("right_motor", .5)
         r.update_position()
         s.draw()
         time.sleep(r.tick_rate)
