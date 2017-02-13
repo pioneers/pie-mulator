51fcac23df67ac205e6ea815989453a9ace77544
chuen0630 <WingChuenLam@pioneers.berkeley.edu>
2017-02-12 16:33:55 -0800

    fix arcade drive implementation

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ def loop():
     Robot.set_value("right_motor", Gamepad.get_value("joystick_right_y"))
     """Arcade Drive"""
     # turningSpeed =  Gamepad.get_value("joystick_left_x")
-    # Robot.set_value("left_motor", -(Gamepad.get_value("joystick_left_y") + turningSpeed))
-    # Robot.set_value("right_motor", Gamepad.get_value("joystick_left_y") - turningSpeed)
+    # left_y = Gamepad.get_value("joystick_left_y")
+    # turningSpeed = turningSpeed * abs(turningSpeed)
+    # left_y = left_y * abs(left_y)
+    # Robot.set_value("left_motor", max(min(-(left_y + turningSpeed), 1.0), -1.0))
+    # Robot.set_value("right_motor", max(min(left_y - turningSpeed, 1.0), -1.0))
 
 
 # Do not delete the line below: it runs the simulator!
