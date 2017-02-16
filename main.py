9a649e3011aad8321dd851d0c65fa3ab20b00034
Scott Numamoto <scott.numamoto@gmail.com>
2017-02-15 22:06:42 -0800

    Easier drive switching

--- main.py
+++ main.py
@@ -14,19 +14,25 @@ def setup():
 def loop():
     if Robot.is_running(drive_fwd):
         return # wait until it completes
-    """Driving straight. """
-    # Robot.set_value("left_motor", 0.7)
-    # Robot.set_value("right_motor", -0.7)
-    """Tank Drive"""
-    # Robot.set_value("left_motor", Gamepad.get_value("joystick_left_y"))
-    # Robot.set_value("right_motor", -Gamepad.get_value("joystick_right_y"))
-    """Arcade Drive"""
-    turningSpeed =  Gamepad.get_value("joystick_left_x")
-    left_y = -Gamepad.get_value("joystick_left_y")
-    turningSpeed = turningSpeed * abs(turningSpeed)
-    left_y = left_y * abs(left_y)
-    Robot.set_value("left_motor", max(min(-(left_y + turningSpeed), 1.0), -1.0))
-    Robot.set_value("right_motor", max(min(left_y - turningSpeed, 1.0), -1.0))
+
+    driving_mode = 0
+
+    if driving_mode = 0:
+        """Driving straight. """
+        # Robot.set_value("left_motor", 0.7)
+        # Robot.set_value("right_motor", -0.7)
+    elif driving_mode = 1:
+        """Tank Drive"""
+        # Robot.set_value("left_motor", Gamepad.get_value("joystick_left_y"))
+        # Robot.set_value("right_motor", -Gamepad.get_value("joystick_right_y"))
+    elif driving_mode = 2:
+        """Arcade Drive"""
+        turningSpeed =  Gamepad.get_value("joystick_left_x")
+        left_y = -Gamepad.get_value("joystick_left_y")
+        turningSpeed = turningSpeed * abs(turningSpeed)
+        left_y = left_y * abs(left_y)
+        Robot.set_value("left_motor", max(min(-(left_y + turningSpeed), 1.0), -1.0))
+        Robot.set_value("right_motor", max(min(left_y - turningSpeed, 1.0), -1.0))
 
 
 # Do not delete the line below: it runs the simulator!
