ffaeb0eed4cdf6d2d254b592df80441b636d5985
Scott Numamoto <scott.numamoto@gmail.com>
2017-03-11 01:19:56 -0800

    Clear the template code

--- main.py
+++ main.py
@@ -2,30 +2,21 @@ from pimulator import Robot, Gamepad, Simulator
 
 
 def teleop_setup():
-    Robot.set_value("left_motor", "duty_cycle", 1)
-    Robot.set_value("right_motor", "duty_cycle", -1)
     pass
 
 
 def teleop_main():
-    driving_mode = 1
+    pass
 
-    if driving_mode == 0:
-        """Driving straight. """
-        Robot.set_value("left_motor", "duty_cycle", 0.7)
-        Robot.set_value("right_motor", "duty_cycle", -0.7)
-    elif driving_mode == 1:
-        """Tank Drive"""
-        Robot.set_value("left_motor", "duty_cycle", Gamepad.get_value("joystick_left_y"))
-        Robot.set_value("right_motor", "duty_cycle", -Gamepad.get_value("joystick_right_y"))
-    elif driving_mode == 2:
-        """Arcade Drive"""
-        turningSpeed = Gamepad.get_value("joystick_left_x")
-        left_y = -Gamepad.get_value("joystick_left_y")
-        turningSpeed = turningSpeed * abs(turningSpeed)
-        left_y = left_y * abs(left_y)
-        Robot.set_value("left_motor", "duty_cycle", max(min(-(left_y + turningSpeed), 1.0), -1.0))
-        Robot.set_value("right_motor", "duty_cycle", max(min(left_y - turningSpeed, 1.0), -1.0))
+
+
+
+
+
+
+
+
+
 
 
 # Do not delete the line below: it runs the simulator!
